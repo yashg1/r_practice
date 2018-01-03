abf354cc149d1ab0f6a4c8b2fe4acede472b6428
yashg1 <gyash@protonmail.com>
2018-01-03 07:19:48 -0500

    Gapminder pop dataset practice from SW

---
+++ gapminder_pop.R
@@ -0,0 +1,25 @@
+library("ggplot2")
+rm(list = setdiff(ls(), lsf.str())) #Clear All
+gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv",header = TRUE)
+#str(gapminder)
+# seAsia <- c("Myanmar","Thailand","Cambodia","Vietnam","Laos")
+# country_vec <- unique(gapminder[,1])
+# seAsia_indices <- country_vec %in% seAsia
+# which(seAsia_indices == TRUE)
+# country_vec[seAsia_indices]
+# conti_list <- unique(gapminder$continent)
+# for (i in conti_list) 
+# {
+# average_age <- 0
+# print(i)
+# age <- gapminder[gapminder$continent == i,"lifeExp"]
+# average_age <- mean(age)
+# #print(average_age)
+# if(average_age>50){
+# #print(i)
+# print(average_age)
+# }
+# }
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(aes(color=continent),shape = 18) + scale_x_log10() +
+  geom_smooth(method="lm", size=1.5) 
