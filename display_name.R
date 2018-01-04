e76f3152d92289b1fa43ed4f0228fed90d388e0d
yashg1 <gyash@protonmail.com>
2018-01-04 17:07:38 -0500

    Work 1/4 afternoon. Updated display_name.R function and fpl_csv_read.R program

--- display_name.R
+++ display_name.R
@@ -5,16 +5,26 @@ display_name <- function(p){
   last_name <- as.character(p$Surname)
   duplicated_index <- which(duplicated(last_name))
   #duplicated_name <- last_name[duplicated(last_name)]
-  first_charac = substr(first_name,1,1)
+  first_charac = substr(first_name,1,3)
   ##
   dis_name <- vector(mode='character', length=nrow(p))
   ##
-  dis_name[first_name %in% ""] <- last_name[first_name %in% ""]
-  ##
-  dis_name[duplicated_index] <- paste(first_charac[duplicated_index],last_name[duplicated_index], sep = ".")
-  next_duplicate <- duplicated_index-1
-  dis_name[next_duplicate] <- paste(first_charac[next_duplicate],last_name[next_duplicate], sep = ".")
-  ##
-  dis_name[dis_name %in% ""] <- last_name[dis_name %in% ""] 
-  return(dis_name)
+  dis_name <- paste(first_name,last_name, sep = "")
+  temp <- str_replace_all(dis_name,fixed(" "), "")
+  temp <- str_replace_all(temp,"[[:punct:]]", "")
+  dis_name <- str_to_lower(temp)
+  p <- cbind(dis_name,p)
+  new <- list("dis_name" = dis_name,"new_list" = p)
+  return(new)
 }
+  
+  ## Old Code
+  #dis_name[first_name %in% ""] <- last_name[first_name %in% ""]
+  ##
+  # dis_name[duplicated_index] <- paste(first_charac[duplicated_index],last_name[duplicated_index], sep = ".")
+  # next_duplicate <- duplicated_index-1
+  # dis_name[next_duplicate] <- paste(first_charac[next_duplicate],last_name[next_duplicate], sep = ".")
+  ##
+  #dis_name[dis_name %in% ""] <- last_name[dis_name %in% ""] 
+  
+
--- fpl_historic_csv_read.R
+++ fpl_historic_csv_read.R
@@ -1,27 +1,42 @@
 # Read multipled CSV files and convert to a list
 rm(list = setdiff(ls(), lsf.str())) #Clear All
-
+#Packages
+library("stringr")
+library("ggplot2")
+##
 filepath_1 <-  paste(getwd(),"/FPL_16/", sep ="")
 fileappend <- "FPL16-GW"
 #fileiter <- 37
 fileext <- ".csv"
-file_vec_1 <- c(filepath_1,fileappend,05,fileext)
-file_vec_2 <- c(filepath_1,fileappend,06,fileext)
-file_vec_3 <- c(filepath_1,fileappend,37,fileext)
-
+file_vec_1 <- c(filepath_1,fileappend,"05",fileext)
+file_vec_2 <- c(filepath_1,fileappend,"06",fileext)
+file_vec_3 <- c(filepath_1,fileappend,"37",fileext)
+##
 myfilename_1 <- paste(file_vec_1,collapse = "")
 myfilename_2 <- paste(file_vec_2,collapse = "")
 myfilename_3 <- paste(file_vec_3,collapse = "")
-
-players_1 <- read.csv(myfilename_1, header= TRUE )
-players_2 <- read.csv(myfilename_2, header= TRUE)
-
+myfilename <- c(myfilename_1,myfilename_2,myfilename_3)
+names(myfilename) <- c("f1","f2","f3")
+##
+gw5 <- read.csv(myfilename[1], header= TRUE )
+gw6 <- read.csv(myfilename[2], header= TRUE)
+gw7 <- read.csv(myfilename[3], header= TRUE)
+game_week <- list(gw5,gw6,gw7)
+names(game_week) <- c('gw-5','gw-6','gw-7')
 source("display_name.R")
-web_name <- display_name(players)
+# web_name = list(wn1 = rep("0",nrow(gw5)),
+#                       wn2 = rep("0",nrow(gw6) ),
+#                       wn3 = rep("0",nrow(gw7))
+# )
+# game_week <- display_name(players$p1)
+# web_name$wn2 <- display_name(players$p2)
+# web_name$wn3 <- display_name(players$p3)
+#extra_elem_index <- which(!web_name$wn3 %in% web_name$wn1)
+
 # Open Multiple CSV files
-filepath_2 <-  paste(getwd(),"/FPL_16/", sep ="")
-temp = list.files(filepath_2,pattern="*.csv")
-myfiles = lapply(temp[1:3], read.csv)
+# filepath_2 <-  paste(getwd(),"/FPL_16/", sep ="")
+# temp = list.files(filepath_2,pattern="*.csv")
+# myfiles = lapply(temp[1:3], read.csv)
 #setwd(filepath_2)
 # for (i in 1:3){
 #   assign(temp[i], read.csv(temp[i]))
