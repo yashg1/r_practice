514d978d3a6349baaf24de62b037029230604c89
yashg1 <gyash@protonmail.com>
2018-01-05 17:21:41 -0500

    Work 1/5 afternoon. Created Useful links text file.

--- fpl_csv_read.R
+++ fpl_csv_read.R
@@ -7,12 +7,13 @@ require("purrr")
 require("tidyr")
 require("dplyr")
 require("stringr")
+setwd("~/R_tutorials/r_practice")
+source("display_name.R")
 
-source("D:/Grad School/Coursework/Extra/R/Tutorials/r_practice/display_name.R")
-
-##
+## Set WD to locate csv files
 wd_now <-  paste(getwd(),"/FPL_16/", sep ="")
-#setwd(wd_now)
+setwd(wd_now)
+#file.path("..","FPL_16/")
 files <- list.files(pattern = "csv$") 
 #files <- files[1:6]
 dat <- map(files, read.csv)
@@ -22,10 +23,24 @@ names(dat) <- f2
 
 # Combine Display Names
 game_week <- lapply(dat, display_name)
+# Extract Points from Game_Week list
+#lapply applies a function to each data frame in the list sequentially
 # game_week$GW05[game_week$GW05$Surname %in% "Cech","TotalPoints"]
+#points_data <- lapply(game_week,function(x) x[x$Surname %in% "Xhaka","TotalPoints"])
+# points_df <- data.frame(unlist(points_data))
+# p <- ggplot(points_df, aes(names(points_data),points_df[[1]]))
+# p + geom_bar(stat = "identity")
 
-points_data <- lapply(game_week,function(x) x[x$Surname %in% "Xhaka",8])
-points_df <- data.frame(unlist(points_data))
-p <- ggplot(points_df, aes(names(points_data),points_df[[1]]))
-p + geom_bar(stat = "identity")
-               
+# Dataframe Manipulation 2
+#a <- lapply(game_week,select(dis_name,PositionsList,Team,Cost,TotalPoints))
+#a <- game_week$GW05 %>% select(dis_name,PositionsList,Team,Cost,TotalPoints)
+#b <- game_week$GW05 %>% group_by(Team) %>% summarise(mean_pts = mean(TotalPoints))
+
+player_name <- str_to_lower("fabianski") # Do Not Enter a single character
+
+player_points_allGW <- map(game_week,~filter(.x,grepl(player_name,dis_name)))%>% 
+                       map(~select(.x, TotalPoints)) 
+player_points_allGW_vec <- unlist(player_points_allGW)
+ppw <- player_points_allGW_vec[2:length(player_points_allGW_vec)] - player_points_allGW_vec[1:(length(player_points_allGW_vec)-1)]
+ppw <- c(player_points_allGW_vec[1],ppw)
+plot(ppw)
--- gapminder_pop.R
+++ gapminder_pop.R
@@ -1,6 +1,8 @@
-library("ggplot2")
-library("plyr")
+require("ggplot2")
+#require("plyr")
+require("dplyr")
 rm(list = setdiff(ls(), lsf.str())) #Clear All
+setwd("~/R_tutorials/r_practice")
 gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv",header = TRUE)
 #str(gapminder)
 # seAsia <- c("Myanmar","Thailand","Cambodia","Vietnam","Laos")
@@ -26,12 +28,35 @@ gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-ga
 #   geom_point(aes(color=continent),shape = 18) + scale_x_log10() +
 #   geom_smooth(method="lm", size=1.5) 
 # Call Function
-source("calcGDP.R")
-withGDP <- calcGDP(gapminder)
-#Data Manipulation
-new <- ddply(
-  .data = gapminder,
-  .variables = c("continent","year"),
-  .fun = function(a) mean(a$lifeExp)
-)
-p2 <- ggplot(data = new[new$continent %in% "Asia",], aes(x = year, y = V1)) + geom_point()
+# source("calcGDP.R")
+# withGDP <- calcGDP(gapminder)
+#Data Manipulation - 1
+# new <- ddply(
+#   .data = gapminder,
+#   .variables = c("continent","year"),
+#   .fun = function(a) mean(a$lifeExp)
+# )
+# p2 <- ggplot(data = new[new$continent %in% "Asia",], aes(x = year, y = V1)) + geom_point()
+#Data Manipulation - 2
+## Using dlplyr package to select, filter data
+lifeExp_country_year <- gapminder %>% filter(continent %in% c("Africa","Oceania"))%>% 
+                        select(year,lifeExp,country)
+#1
+gdp_bycontinents <- gapminder %>%
+  group_by(continent) %>%
+  summarise(a=mean(gdpPercap))
+#2
+lifeExp_bycountry <- gapminder %>% select(country,lifeExp) %>%
+  group_by(country) %>%
+  summarize(mean_lifeExp=mean(lifeExp))
+#3
+gdp_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap))
+#4
+gdp_pop_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap),
+            sd_gdpPercap=sd(gdpPercap),
+            mean_pop=mean(pop),
+            sd_pop=sd(pop))
