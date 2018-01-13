6c02ba99897e745b506aaa4e9410cb9217f56c99
yashg1 <gyash@protonmail.com>
2018-01-12 22:57:23 -0500

    After a long time! Added code to select multiple players

--- fpl_csv_read.R
+++ fpl_csv_read.R
@@ -1,6 +1,7 @@
 #Read CSV files using PURRR / LAPPLY
 rm(list = setdiff(ls(), lsf.str())) #Clear All
 #Packages
+#require("tidyverse")
 require("stringr")
 require("ggplot2")
 require("purrr")
@@ -37,25 +38,29 @@ game_week <- lapply(dat, display_name)
 #a <- game_week$GW05 %>% select(dis_name,PositionsList,Team,Cost,TotalPoints)
 #b <- game_week$GW05 %>% group_by(Team) %>% summarise(mean_pts = mean(TotalPoints))
 
-player_name <- str_to_lower(c("giroud","ozil")) # Do Not Enter a single character
-
+player_name <- str_to_lower(c("ozil","sanchez","aguero")) # Do Not Enter a single character
+## Add code to preserve order - critical!
+pattern <- paste(player_name, collapse = "|")
+n_players <- length(player_name)
 # player_extract_data <- map(game_week,~filter(.x,grepl(player_name,dis_name)))%>% 
 #                        map(~select(.x,PointsLastRound,TotalPoints, EAIndex, Form,NextFixture1 )) 
-player_extract_data <- map(game_week,~filter(.x,grepl(player_name,dis_name)))%>% 
+player_extract_data <- map(game_week,~filter(.x,grepl(pattern,dis_name)))%>% 
   map(~select(.x,PointsLastRound,TotalPoints, EAIndex, Form,NextFixture1 )) 
 
 
 points_per_gw<- as.data.frame(map(player_extract_data,~select(.x,PointsLastRound)))
 
-last_gw <- player_extract_data$GW37$TotalPoints - (player_extract_data$GW37$PointsLastRound) - 
-  player_extract_data$GW36$TotalPoints
+last_gw <- player_extract_data$GW37$TotalPoints - 
+player_extract_data$GW36$TotalPoints
 
-points_per_gw <- as.data.frame(c(points_per_gw,last_gw))
+points_per_gw <- as.data.frame(cbind(points_per_gw,last_gw))
 #names(points_per_gw) <- paste("GW",as.character(4:37),sep = "")
-names(points_per_gw) <- as.character(4:37)
+names(points_per_gw) <- (4:37)
+points_per_gw <- cbind(player_name,points_per_gw)
+  
+a <- points_per_gw %>% gather(gw,pts,-player_name)
 
-
-stack(points_per_gw) %>% ggplot(aes(x = ind, y = values)) + geom_point()
+#stack(points_per_gw) %>% ggplot(aes(x = ind, y = values)) + geom_line()
 
 
 
